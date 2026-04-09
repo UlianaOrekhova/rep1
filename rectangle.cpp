79afe5b171d1b19a7795a92dff4dbcace4a9b31b
204711200 <vasutkinaleksandr4@gmail.com>
2026-04-09 22:31:19 +0700

    .

---
+++ rectangle.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+double calculatePerimeter(double a, double b) {
+    return 2 * (a + b);
+}
+
+double calculateArea(double a, double b) {
+    return a * b;
+}
+
+double calculateDiagonal(double a, double b) {
+    return sqrt(a * a + b * b);
+}
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+
+    double a, b;
+
+    cout << "Введите длину и ширину прямоугольника через пробел: ";
+    cin >> a >> b;
+
+    // Проверка на корректность ввода (стороны должны быть больше нуля)
+    if (a <= 0 || b <= 0) {
+        cout << "Ошибка: стороны прямоугольника должны быть положительными числами." << endl;
+        return 1;
+    }
+
+    cout << "---------------------------------" << endl;
+    cout << "Периметр: " << calculatePerimeter(a, b) << endl;
+    cout << "Площадь: " << calculateArea(a, b) << endl;
+    cout << "Длина диагонали: " << calculateDiagonal(a, b) << endl;
+
+    return 0;
+}

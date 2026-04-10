ad77f73c7704cefff5e2df725c511d6e0ee6db15
Frauzz <anton.shtoyko@mail.ru>
2026-04-10 11:54:10 +0700

    Add files

---
+++ rectangle.cpp
@@ -0,0 +1,16 @@
+#include <cmath>
+#include <cmath.h>
+double RectPerim (double lng, double height)
+{
+return 2*(lng + height);
+}
+double RectSquar (double lng, double height)
+{
+return lng*height;
+}
+double RectLgOfDiag (double lng, double height)
+{
+return sqrt(pow(lng,2)+pow(height,2));
+}
+
+
